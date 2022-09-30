467f472152f8f7a83b08954eeed8477a559104a2
Amal K K <kkamalkk1994@gmail.com>
2022-09-30 07:30:43 +0530

    menu fix

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom"
 
 const LinkCard = ({ route, description }) => {
   return (
-    <li className="bg-emerald-600 text-white w-20 h-20 p-20 flex items-center justify-center hover:bg-emerald-900">
+    <li>
       <Link to={route} className="text-center text-xl font-black">
-        {description}
+        <div className="bg-emerald-600 text-white w-20 h-20 p-20 flex items-center justify-center hover:bg-emerald-900">
+          {description}
+        </div>
       </Link>
     </li>
   )
